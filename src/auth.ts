b68e05252e8b147917dbbb43a5d26dc5b7eb8dd5
Nlkomaru <nikomaru@nikomaru.dev>
2024-06-06 06:31:55 +0900

    Add refresh token method

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,5 @@
 import {JWT} from "next-auth/jwt";
-import NextAuth, {Session} from "next-auth";
+import NextAuth, {Session, User} from "next-auth";
 
 interface MyToken extends JWT {
     accessToken?: string;
@@ -14,9 +14,6 @@ export interface ExtendedSession extends Session {
         uuid?: string | null;
     }
 }
-
-
-
 // @ts-ignore
 export const {handlers, signIn, signOut, auth} = NextAuth({
     providers: [
@@ -52,19 +49,77 @@ export const {handlers, signIn, signOut, auth} = NextAuth({
         },
     ],
     callbacks: {
-        async jwt({ token, account }) {
+        async jwt({ token, account, profile}) {
             // 初回サインイン時に accessToken をトークンに保存
             if (account) {
-                token.accessToken = account.access_token;
+                // First login, save the `access_token`, `refresh_token`, and other
+                // details into the JWT
+                console.log("account is", account)
+
+                const userProfile: User = {
+                    id: profile!.id as string,
+                    name: profile!.username as string,
+                    email: "",
+                    image: "https://crafthead.net/avatar/" + profile!.id as string,
+                }
+
+
+                return {
+                    access_token: account.access_token,
+                    expires_at: token.exp as number,
+                    refresh_token: account.refresh_token,
+                    user: userProfile,
+                }
+            } else if (Date.now() < token.exp! * 1000) {
+                // Subsequent logins, if the `access_token` is still valid, return the JWT
+                return token
+            } else {
+                // Subsequent logins, if the `access_token` has expired, try to refresh it
+                if (!token.refresh_token) throw new Error("Missing refresh token")
+                console.log("refreshing token")
+                try {
+                    // The `token_endpoint` can be found in the provider's documentation. Or if they support OIDC,
+                    // at their `/.well-known/openid-configuration` endpoint.
+                    // i.e. https://accounts.google.com/.well-known/openid-configuration
+                    const response = await fetch(process.env.SERVER_URL + "/oauth2/token", {
+                        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                        body: new URLSearchParams({
+                            grant_type: "refresh_token",
+                            client_id: process.env.CLIENT_ID!,
+                            redirect_uri: process.env.SELF_URL!,
+                            refresh_token: token.refresh_token as string, // 型アサーションを使用してstring型として扱う
+                        }),
+                        method: "POST",
+                    })
+
+                    const responseTokens = await response.json()
+
+                    if (!response.ok) throw responseTokens
+
+                    console.log("refreshed token", responseTokens)
+                    return {
+                        // Keep the previous token properties
+                        ...token,
+                        access_token: responseTokens.access_token,
+                        expires_at: Math.floor(Date.now() / 1000 + (responseTokens.expires_in as number)),
+                        // Fall back to old refresh token, but note that
+                        // many providers may only allow using a refresh token once.
+                        refresh_token: responseTokens.refresh_token ?? token.refresh_token,
+                    }
+                } catch (error) {
+                    console.error("Error refreshing access token", error)
+                    // The error property can be used client-side to handle the refresh token error
+                    return { ...token, error: "RefreshAccessTokenError" as const }
+                }
             }
-            return token;
         },
         async session({session, token}: { session: ExtendedSession; token: MyToken}) {
-            session.accessToken = token.accessToken;
-            session.user = {
-                ...session.user,
+            if (token.user) {
+                session.user = token.user as User
+                session.accessToken = token.access_token as string
             }
-            return session;
+
+            return session
         },
     },
 });
